b6c8ad122859330359d653bc09901790ea6c82d5
Shekha32 <alexshekha32@gmail.com>
2022-10-17 18:25:41 +0300

    Palindrome Number

---
+++ Easy/Palindrome Number/solution.py
@@ -0,0 +1,20 @@
+#Palindrome Number
+class Solution:
+
+	def isPalindrome(self, x: int) -> bool:
+        
+		#print ( str ( x ) )
+		#print ( str ( x ) [::-1] )
+        
+        	return True if str ( x ) == str ( x ) [::-1] else False
+        
+
+def main ():
+
+	ts = Solution ()
+
+	assert ts.isPalindrome ( 121 ) == True
+	assert ts.isPalindrome ( -121 ) == False
+	assert ts.isPalindrome ( 10 ) == False
+
+main ()
