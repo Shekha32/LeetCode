984c871ef6af820ae6a98ec430c3fe60741cba48
Shekha32 <alexshekha32@gmail.com>
2022-10-18 11:51:37 +0300

    Add Binary

---
+++ Easy/Add Binary/solution.py
@@ -0,0 +1,16 @@
+#Add Binary
+class Solution:
+
+    def addBinary ( self, a: str, b: str ) -> str:
+
+        return bin ( int ( a, 2 ) + int ( b, 2 ) ) [2:]
+
+
+def main ():
+
+	ts = Solution ()
+
+	assert ts.addBinary ( "11", "1" ) == "100"
+	assert ts.addBinary ( "1010", "1011" ) == "10101"
+
+main ()
