caaa0ced1295bf50eb794d61a42df04ad367ec36
Shekha32 <alexshekha32@gmail.com>
2022-10-20 12:58:11 +0300

    First Bad Version

---
+++ Easy/First Bad Version/solution.py
@@ -0,0 +1,22 @@
+#First Bad Version
+
+#The isBadVersion API is already defined for you.
+#def isBadVersion(version: int) -> bool:
+
+class Solution:
+
+	def firstBadVersion(self, n: int) -> int:
+        
+		#Binary Search
+		left, right = 0, n - 1
+		
+		while left <= right:
+		
+			mid = ( left + right ) // 2
+		
+			if isBadVersion ( mid ):
+				right = mid - 1
+			else:
+				left = mid + 1
+		
+		return left
